00654beaed502980b24682a6cf2d1dc18348a2bf
Adriane James Jabagat Puzon <adrianejames03@gmail.com>
2023-11-02 10:47:27 +0800

    change variable names

--- Program.cs
+++ Program.cs
@@ -1,45 +1,41 @@
-﻿namespace Merge_Sort_AAC
+﻿namespace KMerge_Sort_AAC
 {
   class Program
   {
     static void Main(string[] args)
     {
-      int[] unsortedArray = { 7, 3, 8, 5, 10, 6, 4, 2, 3, 1 };
-      ShowArray("Unsorted Array", unsortedArray);
-
-      int[] sortedArray = CheckArray(unsortedArray);
-      ShowArray("Sorted Array", sortedArray);
+      int[] prevArray = { 7, 3, 8, 5, 10, 6, 4, 2, 3, 1 };
+      DisplayArray("prevArray", prevArray);
       
-      Console.WriteLine("===== END =====");
+      Console.WriteLine("======== START SORTING ========");
+
+      int[] nextArray = CheckArray(prevArray);
+      DisplayArray("nextArray", nextArray);
+      
+      Console.WriteLine("======== SORTING END ========");
       Console.ReadLine();
     }
 
-    static void ShowArray(string prefix, int[] array)
+    static void DisplayArray(string text, int[] array)
     {
-      Console.Write(prefix + ": ");
-
-      int counter = 0;
+      Console.Write(text + ": ");
+      
       foreach (int i in array)
       {
-        if (counter < array.Length)
-          Console.Write(i + ", ");
-        else
-          Console.Write(i);
-        
-        counter++;
+        Console.Write(i + ", ");
       }
       Console.WriteLine("\n");
     }
 
     static int[] CheckArray(int[] array)
     {
-      Console.WriteLine("--- Check ---");
+      Console.WriteLine("==== Check ====");
       if (array.Length > 1)
       {
         int half = array.Length / 2;
 
-        int[] leftArray = GetHalfArray(array, 0, half, "Left");
-        int[] rightArray = GetHalfArray(array, half, array.Length, "Right");
+        int[] leftArray = SplitArray(array, 0, half, "Left");
+        int[] rightArray = SplitArray(array, half, array.Length, "Right");
 
         leftArray = CheckArray(leftArray);
         rightArray = CheckArray(rightArray);
@@ -50,11 +46,11 @@
       return array;
     }
 
-    static int[] GetHalfArray(int[] oldArray, int from, int to, string side)
+    static int[] SplitArray(int[] oldArray, int from, int to, string side)
     {
-      Console.WriteLine("--- Half Array ---");
+      Console.WriteLine("--- Split Array ---");
       int[] newArray = new int[to - from];
-      Console.WriteLine("New Size[" + side + "]:" + newArray.Length);
+      Console.WriteLine("New Size [" + side + "] :" + newArray.Length);
       int counter = 0;
       for (int i = from; i < to; i++)
       {
@@ -62,12 +58,12 @@
         counter++;
       }
 
-      ShowArray("newArray[" + side + "]", newArray);
+      DisplayArray("newArray [" + side + "] ", newArray);
       return newArray;
     }
 
     static int[] MergeArray(int[] leftArray, int[] rightArray) {
-      Console.WriteLine("--- Merge ---");
+      Console.WriteLine("==== Merge ====");
 
       int[] mergedArray = new int[leftArray.Length + rightArray.Length];
 
@@ -79,11 +75,10 @@
       {
         Console.Write(leftArray[leftIndex] + " > " + rightArray[rightIndex]);
         
-        // replace > with < so it'll become descending
         if (leftArray[leftIndex] > rightArray[rightIndex])
         {
           mergedArray[mergeIndex] = rightArray[rightIndex];
-          Console.WriteLine(" is true, so insert " + rightArray[rightIndex]);
+          Console.WriteLine(" is True, so insert " + rightArray[rightIndex]);
 
           rightIndex++;
           mergeIndex++;
@@ -91,7 +86,7 @@
         else
         {
           mergedArray[mergeIndex] = leftArray[leftIndex];
-          Console.WriteLine(" is false, so insert " + leftArray[leftIndex]);
+          Console.WriteLine(" is False, so insert " + leftArray[leftIndex]);
 
           leftIndex++;
           mergeIndex++;
@@ -101,7 +96,7 @@
       while (leftIndex < leftArray.Length)
       {
         mergedArray[mergeIndex] = leftArray[leftIndex];
-        Console.WriteLine("insert: "+ leftArray[leftIndex]);
+        Console.WriteLine("Insert: "+ leftArray[leftIndex]);
 
         leftIndex++;
         mergeIndex++;
@@ -110,13 +105,13 @@
       while (rightIndex < rightArray.Length)
       {
         mergedArray[mergeIndex] = rightArray[rightIndex];
-        Console.WriteLine("insert: "+ rightArray[rightIndex]);
+        Console.WriteLine("Insert: "+ rightArray[rightIndex]);
         
         rightIndex++;
         mergeIndex++;
       }
       
-      ShowArray("MergeArray", mergedArray);
+      DisplayArray("MergeArray", mergedArray);
       return mergedArray;
     }
   }
